e8a79effd70def0e5933f38141ea621863b2cd27
Gulshid Zada <gulshidzada4@gmail.com>
2026-08-04 18:48:14 +0500

    New Project started ===> Zypto

---
+++ zypto-app/zypto-app/zypto_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  zypto_appApp.swift
+//  zypto-app
+//
+//  Created by Gulshid Zada on 04/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct zypto_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ zypto-app/zypto-appTests/zypto_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  zypto_appTests.swift
+//  zypto-appTests
+//
+//  Created by Gulshid Zada on 04/08/2026.
+//
+
+import Testing
+@testable import zypto_app
+
+struct zypto_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
